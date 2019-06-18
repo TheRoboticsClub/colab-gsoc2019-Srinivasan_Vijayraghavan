1ca047cbbe5853a4ea3312c0ddab597febbb3f15
rinz13r <srinivasan.vijayraghavan@iiitb.org>
2019-06-18 10:01:28 +0530

    Added explicit order of files to build runtime

--- build_runtime.py
+++ build_runtime.py
@@ -2,7 +2,8 @@ import shutil
 import glob
 
 if __name__ == '__main__':
+	files = ['runtime/__PyStr__.js', 'runtime/__PyNone__.js', 'runtime/__PyFunction__.js', 'runtime/utils.js', 'runtime/__PyFloat__.js', 'runtime/__PyTuple__.js', 'runtime/__PyInt__.js', 'runtime/__PyRange__.js', 'runtime/__PyBool__.js', 'runtime/builtin.js', 'runtime/__PyList__.js']
 	with open('runtime.js', 'wb') as outfile:
-		for filename in glob.glob('runtime/*.js'):
+		for filename in files:
 			with open(filename, 'rb') as readfile:
 				shutil.copyfileobj(readfile, outfile)
