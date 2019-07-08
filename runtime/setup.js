4a95be70744136133b6815d025551dd288ee845a
rinz13r <srinivasan.vijayraghavan@iiitb.org>
2019-07-08 23:01:27 +0530

    Proxy Array added

--- runtime/setup.js
+++ runtime/setup.js
@@ -1,1 +1,24 @@
 Function.prototype.__eq__ = function (other) {return __getbool__ (this == other);}
+
+function make_array (init = []) {
+	return new Proxy (init, {
+		get (target, key, recv) {
+			var id = Number (key);
+			if (!isNaN (id)) {
+				if (id < 0) {id += target.length;}
+				console.log (key);
+				return Reflect.get(target, id, recv);
+			}
+            return Reflect.get(target, key, recv);
+		},
+		set (target, key, value, recv) {
+			var id = Number (key);
+			if (!isNaN (id)) {
+				if (id < 0) {id += target.length;}
+				console.log (key);
+				return Reflect.set(target, id, recv);
+			}
+            return Reflect.set (target, key, value, recv);
+		}
+	});
+}
