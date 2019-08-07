b9a189ec4e2d9f91f3032627028d550c4aa41972
rinz13r <srinivasan.vijayraghavan@iiitb.org>
2019-08-07 18:33:27 +0530

    Added ordering of files in build_runtime

--- build_runtime.py
+++ build_runtime.py
@@ -2,22 +2,13 @@ import shutil
 import glob
 
 if __name__ == '__main__':
-	reserved = ['runtime/setup.js', 'runtime/__PyStr__.js', 'runtime/builtin.js', 'runtime/__PyObject__.js']
-
+	order = ['runtime/setup.js', 'runtime/__PyObject__.js', 'runtime/__PyStr__.js',
+	'runtime/__PyNone__.js', 'runtime/exceptions.js', 'runtime/__PyFunction__.js',
+	'runtime/__PySlice__.js', 'runtime/utils.js', 'runtime/__PyFloat__.js',
+	'runtime/__PyDict__.js', 'runtime/__PyTuple__.js', 'runtime/__PyInt__.js',
+	'runtime/__PyRange__.js', 'runtime/__PyBool__.js', 'runtime/__PyModule__.js',
+	'runtime/__PyList__.js', 'runtime/builtin.js']
 	with open('runtime.js', 'wb') as outfile:
-		with open ('runtime/setup.js', 'rb') as readfile:
-			shutil.copyfileobj (readfile, outfile)
-
-		with open ('runtime/__PyObject__.js', 'rb') as readfile:
-			shutil.copyfileobj (readfile, outfile)
-
-		with open ('runtime/__PyStr__.js', 'rb') as readfile:
-			shutil.copyfileobj (readfile, outfile)
-
-		for filename in glob.glob('runtime/*.js'):
-			if (filename not in reserved):
-				with open(filename, 'rb') as readfile:
-					shutil.copyfileobj(readfile, outfile)
-
-		with open ('runtime/builtin.js', 'rb') as readfile:
-			shutil.copyfileobj (readfile, outfile)
+		for filename in order:
+			with open(filename, 'rb') as readfile:
+				shutil.copyfileobj(readfile, outfile)
